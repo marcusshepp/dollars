c5987390a7af07756fd42ef008dcbdd1ef336218
sheph2mj <sheph2mj@cmich.edu>
2016-02-17 10:19:10 -0500

    working test

--- receipt/tests.py
+++ receipt/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Item
+
+
+class ReceiptTests(TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def test_init_items(self):
+        """ tests that the initial items created in the migration file actually exist. """
+        coffee = Item.objects.get(name="Coffee")
+        self.assertTrue(coffee)
+        if self.assertTrue(coffee):
+            print "Item: Coffee, exists."
