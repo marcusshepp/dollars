baa4b42c2749a8a2ed3cc72cea6f9562812d95d4
marcusshepp <sheph2mj@cmich.edu>
2016-02-26 22:08:29 -0500

    options btn now in update dom function

--- receipt/static/js/dyliams.js
+++ receipt/static/js/dyliams.js
@@ -32,7 +32,8 @@ function update_dom(){
           item_markup += '<form id="' + id + '" class="item col-sm-12 col-lg-12" action="api/items/" method="POST">';
           item_markup += '<input type="hidden" name="csrfmiddlewaretoken" value="' + csrf_func() + '" />';
           item_markup += '<div class="pull-left" id="item_' + id + '">' + name + '</div>';
-          item_markup += "<div class='purchase_btn col-sm-4 pull-right' onclick='purchase_item("+id+")'>"+"Purchase"+"</div>"
+          item_markup += '<div class="options pull-right" onclick="show_options(this, '+id+')"><div class="fa fa-arrow-left"></div></div>'
+          item_markup += "<div class='purchase_btn pull-right' onclick='purchase_item("+id+")'>"+"Purchase"+"</div>"
           item_markup += '<span class="times_purchased pull-right"># of purchases: ' + times_purchased + '</span>';
           item_markup += '<span class="times_purchased pull-right">$ '+price+'</span>'
           item_markup += '</form>';
