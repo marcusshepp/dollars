a759378056a643850c87d3daae9ecc3d49fb8092
marcusshepp <sheph2mj@cmich.edu>
2016-03-17 17:54:07 -0400

    :cyclone:

--- as/redo.sh
+++ as/redo.sh
@@ -1,10 +1,10 @@
 echo $1
 if [[ $1 -eq "no-boot" ]]; then
     echo "deleting all data w/o booting server..."
-    bash ~/projects/dollars/as/django/reset_db.sh
+    python manage.py load_data delete foo
 else
     echo "deleting all data + booting server..."
-    bash ~/projects/dollars/as/django/reset_db.sh
+    python manage.py load_data delete foo
     bash ~/projects/dollars/as/django/bootserver.sh
 fi
 
