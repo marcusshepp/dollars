472df6679d09f349e8633721c650574112748577
sheph2mj <sheph2mj@cmich.edu>
2016-03-24 16:41:46 -0400

    more work for filter items list

--- receipt/views.py
+++ receipt/views.py
@@ -121,6 +121,10 @@ def items_all_query(user):
     data["catas"]       = cata_name_id_tuple(user)
     return data
 
+def filter_all_items_by_chars(user):
+    items = list()
+    pass
+
 
 class Common(View):
     """
@@ -572,4 +576,7 @@ class FilterItemsEndpoint(Common):
     def get(self, request, *a, **kw):
         # data = dict()
         data = items_all_query(request.user)
+        return JsonResponse(data)
+    def post(self, request, *a, **kw):
+        data = dict()
         return JsonResponse(data)
